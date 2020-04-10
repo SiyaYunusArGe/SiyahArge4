c88f3407814ad2f1fe862f980340c74b15a8ef89
SiyahYunus <63452405+SiyaYunusArGe@users.noreply.github.com>
2020-04-10 14:50:35 +0300

    Add files via upload

---
+++ Rootkit-Tarama-Aracı.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+os.system("apt.get install figlet")
+os.system("clear")
+print("""
+  _____                    _     _      _   _        _____        
+ |  __ \                  | |   | |    (_) | |      / ____|       
+ | |__) |   ___     ___   | |_  | | __  _  | |_    | (___     ___ 
+ |  _  /   / _ \   / _ \  | __| | |/ / | | | __|    \___ \   / __|
+ | | \ \  | (_) | | (_) | | |_  |   <  | | | |_     ____) | | (__ 
+ |_|  \_\  \___/   \___/   \__| |_|\_\ |_|  \__|   |_____/   \___|
+
+""")
+print("""
+Rootkit Tarama Aracina Hosgeldiniz. Created by.SiyahYunus
+""")
+
+os.system("chkrootkit")
+
